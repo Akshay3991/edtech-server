355ec963e5da6c009137e558613e259781cbea3e
akshay <akshaysabharwal92@gmail.com>
2025-01-23 11:47:22 +0530

    middleware auth

--- routes/profile.js
+++ routes/profile.js
@@ -14,9 +14,9 @@ import  {
 //                                      Profile routes
 // ********************************************************************************************************
 // Delet User Account
-router.delete("/deleteProfile", deleteAccount)
-router.put("/updateProfile", updateProfile)
-router.get("/getUserDetails", getAllUserDetails)
+router.delete("/deleteProfile",auth, deleteAccount)
+router.put("/updateProfile", auth ,  updateProfile)
+router.get("/getUserDetails", auth ,  getAllUserDetails)
 // Get Enrolled Courses
 router.get("/getEnrolledCourses", auth, getEnrolledCourses)
 router.put("/updateDisplayPicture", auth, updateDisplayPicture)
