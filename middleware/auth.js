de3e2d15e758ac07d56af47c1099027d1328669d
akshay <akshaysabharwal92@gmail.com>
2025-01-23 20:33:57 +0530

    .

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,99 +9,98 @@ dotenv.config();
 // Authentication Middleware
 const auth = async (req, res, next) => {
   try {
-		// Extracting JWT from request cookies, body or header
-		const token =
-			req.cookies.token ||
-			req.body.token ||
-			req.header("Authorization").replace("Bearer ", "");
+    // Extracting JWT from request cookies, body or header
+    const token =
+      req.cookies.token ||
+      req.body.token ||
+      req.header("Authorization").replace("Bearer ", "");
 
-		// If JWT is missing, return 401 Unauthorized response
-		if (!token) {
-			return res.status(401).json({ success: false, message: `Token Missing` });
-		}
+    // If JWT is missing, return 401 Unauthorized response
+    if (!token) {
+      return res.status(401).json({ success: false, message: `Token Missing` });
+    }
 
-		try {
-			// Verifying the JWT using the secret key stored in environment variables
-			const decode = await jwt.verify(token, process.env.JWT_SECRET);
-			console.log(decode);
-			// Storing the decoded JWT payload in the request object for further use
-			req.user = decode;
-		} catch (error) {
-			// If JWT verification fails, return 401 Unauthorized response
-			return res
-				.status(401)
-				.json({ success: false, message: "token is invalid" });
-		}
+    try {
+      // Verifying the JWT using the secret key stored in environment variables
+      const decode = await jwt.verify(token, process.env.JWT_SECRET);
+      // Storing the decoded JWT payload in the request object for further use
+      req.user = decode;
+    } catch (error) {
+      // If JWT verification fails, return 401 Unauthorized response
+      return res
+        .status(401)
+        .json({ success: false, message: "token is invalid" });
+    }
 
-		// If JWT is valid, move on to the next middleware or request handler
-		next();
-	} catch (error) {
-		// If there is an error during the authentication process, return 401 Unauthorized response
-		return res.status(401).json({
-			success: false,
-			message: `Something Went Wrong While Validating the Token`,
-		});
-	}
+    // If JWT is valid, move on to the next middleware or request handler
+    next();
+  } catch (error) {
+    // If there is an error during the authentication process, return 401 Unauthorized response
+    return res.status(401).json({
+      success: false,
+      message: `Something Went Wrong While Validating the Token`,
+    });
+  }
 };
 
 // Check if the user is a Student
 const isStudent = async (req, res, next) => {
-   try {
-		const userDetails = await User.findOne({ email: req.user.email });
+  try {
+    const userDetails = await User.findOne({ email: req.user.email });
 
-		if (userDetails.accountType !== "Student") {
-			return res.status(401).json({
-				success: false,
-				message: "This is a Protected Route for Students",
-			});
-		}
-		next();
-	} catch (error) {
-		return res
-			.status(500)
-			.json({ success: false, message: `User Role Can't be Verified` });
-	}
+    if (userDetails.accountType !== "Student") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a Protected Route for Students",
+      });
+    }
+    next();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: `User Role Can't be Verified` });
+  }
 };
 
 // Check if the user is an Admin
 const isAdmin = async (req, res, next) => {
   try {
-		const userDetails = await User.findOne({ email: req.user.email });
+    const userDetails = await User.findOne({ email: req.user.email });
 
-		if (userDetails.accountType !== "Admin") {
-			return res.status(401).json({
-				success: false,
-				message: "This is a Protected Route for Admin",
-			});
-		}
-		next();
-	} catch (error) {
-		return res
-			.status(500)
-			.json({ success: false, message: `User Role Can't be Verified` });
-	}
+    if (userDetails.accountType !== "Admin") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a Protected Route for Admin",
+      });
+    }
+    next();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: `User Role Can't be Verified` });
+  }
 };
 
 // Check if the user is an Instructor
 const isInstructor = async (req, res, next) => {
-   try {
-		const userDetails = await User.findOne({ email: req.user.email });
-		console.log(userDetails);
+  try {
+    const userDetails = await User.findOne({ email: req.user.email });
+    console.log(userDetails);
 
-		console.log(userDetails.accountType);
+    console.log(userDetails.accountType);
 
-		if (userDetails.accountType !== "Instructor") {
-			return res.status(401).json({
-				success: false,
-				message: "This is a Protected Route for Instructor",
-			});
-		}
-		next();
-	} catch (error) {
-		return res
-			.status(500)
-			.json({ success: false, message: `User Role Can't be Verified` });
-	}
+    if (userDetails.accountType !== "Instructor") {
+      return res.status(401).json({
+        success: false,
+        message: "This is a Protected Route for Instructor",
+      });
+    }
+    next();
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: `User Role Can't be Verified` });
+  }
 };
 
 export { auth, isStudent, isAdmin, isInstructor };
--- routes/profile.js
+++ routes/profile.js
@@ -1,25 +1,25 @@
-import express from "express"
+import express from "express";
 const router = express.Router();
-import { auth, isInstructor } from "../middleware/auth.js"
-import  {
+import { auth, isInstructor } from "../middleware/auth.js";
+import {
   deleteAccount,
   updateProfile,
   getAllUserDetails,
   updateDisplayPicture,
   getEnrolledCourses,
   instructorDashboard,
-} from "../controllers/profile.js"
+} from "../controllers/profile.js";
 
 // ********************************************************************************************************
 //                                      Profile routes
 // ********************************************************************************************************
 // Delet User Account
-router.delete("/deleteProfile",auth, deleteAccount)
-router.put("/updateProfile", auth ,  updateProfile)
-router.get("/getUserDetails", auth ,  getAllUserDetails)
+router.delete("/deleteProfile", auth, deleteAccount);
+router.put("/updateProfile", auth, updateProfile);
+router.get("/getUserDetails", auth, getAllUserDetails);
 // Get Enrolled Courses
-router.get("/getEnrolledCourses", auth, getEnrolledCourses)
-router.put("/updateDisplayPicture", auth, updateDisplayPicture)
-router.get("/instructorDashboard", auth, isInstructor, instructorDashboard)
+router.get("/getEnrolledCourses", auth, getEnrolledCourses);
+router.put("/updateDisplayPicture", auth, updateDisplayPicture);
+router.get("/instructorDashboard", auth, isInstructor, instructorDashboard);
 
-export default router
+export default router;
