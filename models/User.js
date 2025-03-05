556ed0566efc0553fc2a9db07622e3c00cacacb3
akshay <akshaysabharwal92@gmail.com>
2025-03-05 18:28:54 +0530

    .

--- models/User.js
+++ models/User.js
@@ -68,16 +68,16 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
+// userSchema.pre("save", async function (next) {
+//   if (!this.isModified("password")) return next();
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
+//     next();
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 // Method to compare passwords
 // userSchema.methods.comparePassword = async function (candidatePassword) {
